f9fbd5d67f14536aaf909e6f7364c4c415552e89
Glitch (glitch-hello-website) <none>
2023-08-19 16:20:54 +0000

    🕋🎆 Checkpoint ./testingstuff.py:60784004/1324 ./testingotherstuff.py:60784004/274 ./app.py:60784004/446 ./templates/index.html:60784004/417

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        name = request.form.get('name')
+        email = request.form.get('email')
+        return "Received form data: Name - {name}, Email - {email}"
+    return render_template('index.html')  # Create an HTML template for the form
+
+if __name__ == '__main__':
+    app.run(debug=True)
