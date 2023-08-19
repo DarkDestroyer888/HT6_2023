a9eda5a39a208639426b555e78409dacd4067136
Glitch (glitch-hello-website) <none>
2023-08-19 16:31:55 +0000

    🐽⛽️ Checkpoint ./app.py:60784004/33

--- app.py
+++ app.py
@@ -11,4 +11,4 @@ def index():
     return render_template('index.html')  # Create an HTML template for the form
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True,port=8080,host='127.0.0.1')
